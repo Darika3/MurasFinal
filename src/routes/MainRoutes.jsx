080646be513722d20e01c10020c4b217db4dfe58
Пользователь <karabaevameerim955@gmail.com>
2023-06-04 16:17:29 +0600

    .

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -13,11 +13,12 @@ import Tours from "../components/Tours/Tours";
 import Cart from "../components/Cart/Cart";
 import Auth from "../components/Auth/Auth";
 import EditProduct from "../components/Products/Crud/EditProduct";
-import TrekkingTours from "../components/Tours/TrekkingTours";
 import MultidaysTour from "../components/Tours/MultidaysTour";
+import TrekkingTours from "../components/Tours/TrekkingTours";
+// import Chat from "../components/Chat/Chat";
 import HorseRiding from "../components/Tours/HorseRiding";
 import FavPage from "../FavPage";
-import TrekkingTours from "../components/Tours/TrekkingTours";
+import CreditCard from "../components/creditCard/Card";
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
@@ -27,14 +28,10 @@ const MainRoutes = () => {
     { link: "/clothes", element: <Clothes />, id: 4 },
     { link: "/souvenirs", element: <Souvenirs />, id: 5 },
 
-<<<<<<< HEAD
-    { link: "/trekking", element: <TrekkingTours />, id: 6 },
-=======
     { link: "/trek", element: <TrekkingTours />, id: 6 },
     { link: "/multidays", element: <MultidaysTour />, id: 7 },
-    { link: "/horse", element: <HorseRiding />, id: 16 },
+    { link: "/horse", element: <HorseRiding/>, id: 16 },
 
->>>>>>> 32055b89dc9e8e40048d7741ba830fc1a5aaf3e7
     { link: "/food", element: <ProductFood />, id: 8 },
     { link: "/culture", element: <Culture />, id: 9 },
     { link: "/tours", element: <Tours />, id: 10 },
@@ -42,7 +39,9 @@ const MainRoutes = () => {
     { link: "/admin", element: <AddProduct />, id: 12 },
     { link: "/authtor", element: <Auth />, id: 13 },
     { link: "/edit/:id", element: <EditProduct />, id: 14 },
-    { link: "/fav", element: <FavPage />, id: 15 },
+    { link: "/fav", element: <FavPage/>, id: 15 },
+    // { link: "/chat", element: <Chat/>, id: 17},
+    { link: "/creditCard", element: <CreditCard/>, id: 18},
   ];
   return (
     <>
